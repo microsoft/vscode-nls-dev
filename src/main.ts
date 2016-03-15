303ccf5b899cff10995469451de632f9f335453d
Dirk Baeumer <dirkb@microsoft.com>
2016-03-15 12:59:56 +0100

    Fixed caseing for locale information

--- src/main.ts
+++ src/main.ts
@@ -63,9 +63,9 @@ export function rewriteLocalizeCalls(): ThroughStream {
 }
 
 const iso639_3_to_2 = {
-	'chs': 'zh-cn',
-	'cht': 'zh-tw',
-	'csy': 'cs-cz',
+	'chs': 'zh-CN',
+	'cht': 'zh-TW',
+	'csy': 'cs-CZ',
 	'deu': 'de',
 	'enu': 'en',
 	'esn': 'es',
@@ -76,10 +76,10 @@ const iso639_3_to_2 = {
 	'kor': 'ko',
 	'nld': 'nl',
 	'plk': 'pl',
-	'ptb': 'pt-br',
+	'ptb': 'pt-BR',
 	'ptg': 'pt',
 	'rus': 'ru',
-	'sve': 'sv-se',
+	'sve': 'sv-SE',
 	'trk': 'tr'
 };
 
