aac283fb0a797ddd8a3f13a16ee18b62a54869f8
Johannes Rieken <johannes.rieken@gmail.com>
2018-08-24 16:22:56 +0200

    fix relative path issue

--- src/webpack-loader.ts
+++ src/webpack-loader.ts
@@ -5,15 +5,17 @@
 
 'use strict';
 
-import { processFile } from "./lib";
 import { relative } from "path";
+import { processFile } from "./lib";
 
 /**
  * A [webpack loader](https://webpack.js.org/api/loaders/) that rewrite nls-calls.
  */
 module.exports = function (content, map, meta) {
+	console.assert(this.query && typeof this.query.base === 'string', 'Expected {base: string} option');
+
 	const callback = this.async();
-	const relativePath = relative(this.context, this.resourcePath);
+	const relativePath = relative(this.query.base, this.resourcePath);
 	const result = processFile(content, relativePath, map);
 	if (result.errors && result.errors.length > 0) {
 		// error
